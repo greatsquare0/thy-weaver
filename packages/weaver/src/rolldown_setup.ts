29ae80df2290d6abaf9802344a518cf9d92d68f0
greatsquare0 <greatsquare0@proton.me>
2025-07-06 21:47:36 -0300

    Some fancy logging helpers

--- packages/weaver/src/rolldown_setup.ts
+++ packages/weaver/src/rolldown_setup.ts
@@ -1,50 +1,72 @@
-import pico from "picocolors";
 import { resolve } from "node:path";
 import { cwd } from "node:process";
 
 import postcss from "rollup-plugin-postcss";
 import { swc } from "rollup-plugin-swc3";
 //import url, { type RollupUrlOptions } from "@rollup/plugin-url";
-import type { RolldownOptions, WarningHandlerWithDefault } from "rolldown";
+import type {
+  LogLevel,
+  LogOrStringHandler,
+  RolldownOptions,
+  RollupLog,
+} from "rolldown";
 
 import { loadConfig } from "./config/config_handler.ts";
 import { handleVendorFiles } from "./rolldown_plugins.ts";
+import { fancyLogFormater } from "./utils.ts";
 
 const config = await loadConfig();
 //const mode = process.env.NODE_ENV || "development";
 
-const onwarn: WarningHandlerWithDefault = (warning, log) => {
-  //Silence circular dependency warning
-  if (warning.code === "CIRCULAR_DEPENDENCY") {
-    return undefined;
+const onLog = (
+  level: LogLevel,
+  log: RollupLog,
+  defaultHandler: LogOrStringHandler,
+) => {
+  switch (level) {
+    case "debug":
+      defaultHandler("debug", fancyLogFormater("ROLLDOWN", "DEBUG", log));
+      break;
+
+    case "info":
+      defaultHandler("info", fancyLogFormater("ROLLDOWN", "INFO", log));
+      break;
+
+    case "warn":
+      //Silence circular dependency warning
+      if (log.code === "CIRCULAR_DEPENDENCY") {
+        return undefined;
+      }
+      defaultHandler("warn", fancyLogFormater("ROLLDOWN", "WARN", log));
+
+      break;
+    default:
+      break;
   }
-  log(
-    `\n\n${pico.inverse(pico.bold(" ROLLUP "))} ${pico.bgYellow(
-      pico.bold(" WARN "),
-    )} ${warning.message} \n`,
-  );
 };
 
-export const rolldownOptions: RolldownOptions = {
-  onwarn,
-  input: resolve(cwd(), config.bundler.filesystem!.projectFiles.entryPoint),
+export const setupRolldown = () => {
+  return {
+    onLog,
+    input: resolve(cwd(), config.bundler.filesystem!.projectFiles.entryPoint),
 
-  plugins: [
-    // @ts-expect-error
-    postcss({
-      module: false,
-      plugins: config.bundler.postcss!.plugins,
-      extract: true,
-      //sourceMap: mode === "development",
-      modules: false,
-      autoModules: false,
-      use: {
-        sass: {
-          silenceDeprecations: ["legacy-js-api"],
+    plugins: [
+      // @ts-expect-error
+      postcss({
+        module: false,
+        plugins: config.bundler.postcss!.plugins,
+        extract: true,
+        //sourceMap: mode === "development",
+        modules: false,
+        autoModules: false,
+        use: {
+          sass: {
+            silenceDeprecations: ["legacy-js-api"],
+          },
         },
-      },
-    }),
-    handleVendorFiles(),
-    swc(config.bundler.swc),
-  ],
+      }),
+      handleVendorFiles(),
+      swc(config.bundler.swc),
+    ],
+  } as RolldownOptions;
 };
--- packages/weaver/src/utils.ts
+++ packages/weaver/src/utils.ts
@@ -2,6 +2,8 @@ import { resolve, normalize, sep, posix } from "node:path";
 import { cwd } from "node:process";
 import { outputFile, type PathLike } from "fs-extra/esm";
 import { readFile, stat } from "node:fs/promises";
+import type { RollupLog } from "rolldown";
+import pico from "picocolors";
 
 export const tempFolderPath = () => {
   return resolve(cwd(), "node_modules", ".lib-weaver-temp");
@@ -38,4 +40,71 @@ export const concat = async (files: PathLike[], outputFilePath?: PathLike) => {
 
 export const win2posixPath = (path: string): string => {
   return normalize(path).replaceAll(sep, posix.sep);
+};
+
+type Emiters = "ROLLDOWN" | "TWEENODE" | "DEV SERVER";
+type LogLevel = "INFO" | "WARN" | "DEBUG" | "ERROR" | "PROGRESS";
+
+const colorizeEmiter = (str: Emiters) => {
+  let output = " " + str + " ";
+
+  switch (str) {
+    case "DEV SERVER":
+      output = pico.bgMagenta(pico.white(pico.bold(output)));
+      break;
+
+    case "ROLLDOWN":
+      output = pico.bgWhite(pico.black(pico.bold(output)));
+      break;
+
+    case "TWEENODE":
+      output = pico.bgCyan(pico.white(pico.bold(output)));
+      break;
+  }
+
+  return output;
+};
+
+export const fancyLogFormater = (
+  emiter: Emiters,
+  logLevel: LogLevel,
+  log: RollupLog,
+) => {
+  const emiterLabel = colorizeEmiter(emiter);
+
+  let levelLabel: string | null = " " + logLevel + " ";
+  switch (logLevel) {
+    case "INFO":
+      levelLabel = pico.bgWhite(pico.dim(pico.bold(levelLabel)));
+      break;
+
+    case "WARN":
+      levelLabel = pico.bgYellow(pico.black(pico.bold(levelLabel)));
+      break;
+
+    case "DEBUG":
+      levelLabel = pico.bgWhite(pico.dim(pico.bold(levelLabel)));
+      break;
+
+    case "ERROR":
+      levelLabel = pico.bgRed(pico.white(pico.bold(levelLabel)));
+      break;
+    case "PROGRESS":
+      levelLabel = null;
+      break;
+  }
+
+  const result: string[] = [
+    `${emiterLabel} ${levelLabel == null ? null : levelLabel} ${log.message}`,
+  ];
+
+  log.cause ? result.push(`\n${log.cause}\n`) : null;
+  log.meta ? result.push(`\n${log.meta}\n`) : null;
+  log.stack ? result.push(`\n${log.stack}\n`) : null;
+
+  return result.join("");
+};
+
+export const fancyOraPrefixer = (emiter: Emiters) => {
+  return colorizeEmiter(emiter);
 };
