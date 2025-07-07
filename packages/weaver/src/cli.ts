ac2947ecae3fd9c6db3d959b8b00ccfb945a7775
greatsquare0 <greatsquare0@proton.me>
2025-07-06 22:51:54 -0300

    Started the cli

--- packages/weaver/src/cli.ts
+++ packages/weaver/src/cli.ts
@@ -1,5 +1,39 @@
 #!/usr/bin/env node
+import { program } from "commander";
+import pkg from "../package.json" with { type: "json" };
+const { version, description } = pkg;
+import pico from "picocolors";
 
-console.log("Hello world");
+program
+  .name("weaver")
+  .description(
+    "\t\t" +
+      pico.bgBlue(pico.white(pico.bold("  Thy Weaver  "))) +
+      "\n" +
+      description,
+  )
+  .version(version);
 
+program
+  .command("build")
+  .description("Build the project to dist")
+  .action((_str): any => {
+    return import("./run_build.ts");
+  });
+
+program
+  .command("dev")
+  .description("Start the live dev environment")
+  .action((str) => {
+    console.log(str);
+  });
+
+program
+  .command("package")
+  .description("Build and package the project in to a .zip for distribution")
+  .action((str) => {
+    console.log(str);
+  });
+
+program.parse();
 process.exit(0);
